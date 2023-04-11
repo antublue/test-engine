ed86730f5913a22ecfcfe0fe3a4aa28366e81b24
antublue <antublue@antublue.org>
2023-04-10 23:32:54 -0400

    Made configuration to disable TEST and PASS logging expiremental

--- core/src/main/java/org/antublue/test/engine/TestEngineConstants.java
+++ core/src/main/java/org/antublue/test/engine/TestEngineConstants.java
@@ -22,11 +22,12 @@ package org.antublue.test.engine;
 public final class TestEngineConstants {
 
     public static final String PREFIX = "antublue.test.engine";
+    public static final String EXPERIMENTAL = ".experimental";
     
     public static final String CONSOLE_OUTPUT = PREFIX + ".console.output";
     public static final String THREAD_COUNT = PREFIX + ".thread.count";
-    public static final String LOG_TEST_MESSAGES = PREFIX + ".log.test.messages";
-    public static final String LOG_PASS_MESSAGES = PREFIX + ".log.pass.messages";
+    public static final String LOG_TEST_MESSAGES = PREFIX + EXPERIMENTAL + ".log.test.messages";
+    public static final String LOG_PASS_MESSAGES = PREFIX + EXPERIMENTAL + ".log.pass.messages";
 
     public static final String TEST_CLASS_INCLUDE = PREFIX + ".test.class.include";
     public static final String TEST_CLASS_EXCLUDE = PREFIX + ".test.class.exclude";
