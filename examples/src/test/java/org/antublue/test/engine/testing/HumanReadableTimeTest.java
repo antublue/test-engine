b331338cbf6573fdfc6076f4e08be2508fd154e4
antublue <antublue@antublue.org>
2023-04-25 12:25:20 -0400

    Added test parameter

--- examples/src/test/java/org/antublue/test/engine/testing/HumanReadableTimeTest.java
+++ examples/src/test/java/org/antublue/test/engine/testing/HumanReadableTimeTest.java
@@ -14,6 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class HumanReadableTimeTest {
 
+    private static final String MILLISECONDS = "milliseconds";
+    private static final String HUMAN_READABLE_TIME = "humanReadableTime";
+    
     private long milliseconds;
     private String expectedHumanReadableTime;
 
@@ -22,41 +25,58 @@ public class HumanReadableTimeTest {
         return Stream.of(
                 ParameterMap
                         .named("11969L")
-                        .put("milliseconds", 11969L)
-                        .put("humanReadableTime", "11 seconds, 969 ms")
+                        .put(MILLISECONDS, 11969L)
+                        .put(HUMAN_READABLE_TIME, "11 seconds, 969 ms")
                         .parameter(),
                 ParameterMap
                         .named("1000L")
-                        .put("milliseconds", 1000L)
-                        .put("humanReadableTime", "1 second, 0 ms")
+                        .put(MILLISECONDS, 1000L)
+                        .put(HUMAN_READABLE_TIME, "1 second, 0 ms")
                          .parameter(),
                 ParameterMap
                         .named("1001L")
-                        .put("milliseconds", 1001L)
-                        .put("humanReadableTime", "1 second, 1 ms")
+                        .put(MILLISECONDS, 1001L)
+                        .put(HUMAN_READABLE_TIME, "1 second, 1 ms")
                         .parameter(),
                 ParameterMap
                         .named("60001L")
-                        .put("milliseconds", 60001L)
-                        .put("humanReadableTime", "1 minute, 0 seconds, 1 ms")
+                        .put(MILLISECONDS, 60001L)
+                        .put(HUMAN_READABLE_TIME, "1 minute, 0 seconds, 1 ms")
                         .parameter(),
                 ParameterMap
                         .named("61001L")
-                        .put("milliseconds", 61001L)
-                        .put("humanReadableTime", "1 minute, 1 second, 1 ms")
+                        .put(MILLISECONDS, 61001L)
+                        .put(HUMAN_READABLE_TIME, "1 minute, 1 second, 1 ms")
+                        .parameter(),
+                ParameterMap
+                        .named("3661001L")
+                        .put(MILLISECONDS, 3661001L)
+                        .put(HUMAN_READABLE_TIME, "1 hour, 1 minute, 1 second, 1 ms")
+                        .parameter(),
+                ParameterMap
+                        .named("3721001L")
+                        .put(MILLISECONDS, 3721001L)
+                        .put(HUMAN_READABLE_TIME, "1 hour, 2 minutes, 1 second, 1 ms")
                         .parameter());
     }
 
     @TestEngine.Parameter
     public void parameter(Parameter parameter) {
-        milliseconds = parameter.value(ParameterMap.class).get("milliseconds");
-        expectedHumanReadableTime = parameter.value(ParameterMap.class).get("humanReadableTime");
+        milliseconds = parameter.value(ParameterMap.class).get(MILLISECONDS);
+        expectedHumanReadableTime = parameter.value(ParameterMap.class).get(HUMAN_READABLE_TIME);
     }
 
     @TestEngine.Test
     public void testHumanReadableTime() {
-        System.out.println(String.format("testHumanReadableTime() milliseconds [%d] string [%s]", milliseconds, expectedHumanReadableTime));
-        String humanReadableTime = HumanReadableTime.toHumanReadable(milliseconds);
-        assertThat(humanReadableTime).isEqualTo(expectedHumanReadableTime);
+        String actualHumanReadableTime = HumanReadableTime.toHumanReadable(milliseconds);
+
+        System.out.println(
+                String.format(
+                        "testHumanReadableTime() milliseconds [%d] expected [%s] actual [%s]",
+                        milliseconds,
+                        expectedHumanReadableTime,
+                        actualHumanReadableTime));
+
+        assertThat(actualHumanReadableTime).isEqualTo(expectedHumanReadableTime);
     }
 }
