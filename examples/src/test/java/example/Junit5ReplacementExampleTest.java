9513c472fb52d667a09b42cd60fff00e67b6f398
antublue <antublue@antublue.org>
2023-06-24 10:17:39 -0400

    Optimized imports

--- examples/src/test/java/example/Junit5ReplacementExampleTest.java
+++ examples/src/test/java/example/Junit5ReplacementExampleTest.java
@@ -3,8 +3,6 @@ package example;
 import org.antublue.test.engine.api.TestEngine;
 import org.antublue.test.engine.api.argument.StringArgument;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.stream.Stream;
 
 /**
