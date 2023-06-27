19679bcfdc9d3aebf23521443f2f6b46d8d7da06
antublue <antublue@antublue.org>
2023-06-27 08:59:31 -0400

    Optimized imports

--- examples/src/test/java/example/locking/ClassLockingTest1.java
+++ examples/src/test/java/example/locking/ClassLockingTest1.java
@@ -6,7 +6,6 @@ import org.antublue.test.engine.api.argument.IntegerArgument;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Fail.fail;
