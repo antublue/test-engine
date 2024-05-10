fe92b6364add8568f0b90066631e1085f69611d3
antublue <antublue@antublue.org>
2024-05-10 16:27:13 -0400

    Added StoreException
    Signed-off-by: antublue <antublue@antublue.org>

--- api/src/main/java/org/antublue/test/engine/api/util/Store.java
+++ api/src/main/java/org/antublue/test/engine/api/util/Store.java
@@ -25,7 +25,6 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import org.antublue.test.engine.api.StoreException;
 
 /**
  * Class to implement a Store
