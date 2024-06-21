c56979bf704f2852ab96e3d71648a015c671aeef
antublue <antublue@antublue.org>
2024-06-21 12:08:33 -0400

    Added classes
    Signed-off-by: antublue <antublue@antublue.org>

---
+++ extras/src/main/java/org/antublue/test/engine/extras/throttle/ExponentialBackoffThrottle.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2024 The AntuBLUE test-engine project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.antublue.test.engine.extras.throttle;
+
+/** Class to implement ExponentialBackupThrottle */
+@SuppressWarnings("PMD.EmptyCatchBlock")
+public class ExponentialBackoffThrottle implements Throttle {
+
+    private final long maximumMilliseconds;
+    private long throttleMilliseconds;
+
+    /**
+     * Constructor
+     *
+     * @param minimumMilliseconds minimumMilliseconds
+     * @param maximumMilliseconds maximumMilliseconds
+     */
+    public ExponentialBackoffThrottle(long minimumMilliseconds, long maximumMilliseconds) {
+        this.maximumMilliseconds = maximumMilliseconds;
+        this.throttleMilliseconds = minimumMilliseconds;
+    }
+
+    @Override
+    public void throttle() {
+        System.out.println("throttleMilliseconds [" + throttleMilliseconds + "]");
+
+        try {
+            Thread.sleep(throttleMilliseconds);
+        } catch (InterruptedException e) {
+            // DO NOTHING
+        }
+
+        throttleMilliseconds = throttleMilliseconds * 2;
+        if (throttleMilliseconds > maximumMilliseconds) {
+            throttleMilliseconds = maximumMilliseconds;
+        }
+    }
+}
