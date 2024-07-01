189b43475ed297fc9d33d38469209784e6f54622
antublue <antublue@antublue.org>
2024-06-30 23:32:13 -0400

    Fixed spelling
    Signed-off-by: antublue <antublue@antublue.org>

--- engine/src/main/java/org/antublue/test/engine/internal/discovery/EngineDiscoveryRequestResolver.java
+++ engine/src/main/java/org/antublue/test/engine/internal/discovery/EngineDiscoveryRequestResolver.java
@@ -385,7 +385,7 @@ public class EngineDiscoveryRequestResolver {
 
         List<Argument<?>> testArguments = new ArrayList<>();
 
-        Object object = getArumentSupplierMethod(testClass).invoke(null, (Object[]) null);
+        Object object = getArgumentSupplierMethod(testClass).invoke(null, (Object[]) null);
         if (object == null) {
             return testArguments;
         }
@@ -433,9 +433,9 @@ public class EngineDiscoveryRequestResolver {
      * @param testClass testClass
      * @return the argument supplier method
      */
-    private static Method getArumentSupplierMethod(Class<?> testClass) {
+    private static Method getArgumentSupplierMethod(Class<?> testClass) {
         if (LOGGER.isTraceEnabled()) {
-            LOGGER.trace("getArumentSupplierMethod() testClass [%s]", testClass.getName());
+            LOGGER.trace("getArgumentSupplierMethod() testClass [%s]", testClass.getName());
         }
 
         List<Method> methods =
